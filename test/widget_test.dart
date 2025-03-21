b73ad6f304eb926247e1fc9fa043b05bebb8df5d
Sanzith Alam <121573768+Sanzith-Alam@users.noreply.github.com>
2025-03-21 23:13:32 +0600

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:food_delivery/main.dart';
 
-void main() {
+void main( {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
